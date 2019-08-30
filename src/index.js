75783b862951e73f79349afd85c1337de85b2649
Sivakumar <tak2siva@gmail.com>
2019-08-30 16:56:00 +0530

    Simple boiler plate without hot reload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'React with Webpack and Babel! Hello World!';
+
+ReactDOM.render(
+    <div>{title}</div>,
+    document.getElementById('app')
+);
